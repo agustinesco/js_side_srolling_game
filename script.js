ff34560512a175e492caba081345db3a70e0a96f
agustinesco <agustin.escobar@lambdaclass.com>
2023-12-26 11:40:23 -0300

    Start handlers implementation

---
+++ script.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth - 100;
+    canvas.height = window.innerHeight - 100;
+
+    class InputHandler {
+
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+
+    function handleEnemies(){
+
+    }
+
+    function displayStatusText() {
+        
+    }
+
+    function animate() {
+        
+    }
+});
+
+window.addEventListener("resize", () => {
+    const canvas = document.getElementById("canvas1");
+    canvas.width = window.innerWidth - 100;
+    canvas.height = window.innerHeight - 100;
+})
